7ae5b79f3e203cef985b8605abab260ac558de90
Naval Thakur <53907803+thakurnaval@users.noreply.github.com>
2026-02-25 16:22:49 +0530

    Remove sensitive data from index.tsx
    Removed sensitive information from index.tsx.

--- index.tsx
+++ index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-pwd = "xxxxxYYYYYYZZZZZ12345$$=";
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
